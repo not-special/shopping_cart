94a47d96d0a18cd71e73ed830825e64c4e433adb
Scott Graham <scttgrhm7+public@gmail.com>
2022-06-07 09:41:14 -0700

    Connect client to server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,22 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import ProductListing from "./ProductListing";
 import Cart from "./Cart";
 import AddProductForm from "./AddProductForm";
 import data from "../lib/data.js"
-
-
+import productService from '../services/products'
 
 const App = () => {
-	const [products, setProducts] = useState(data);
+	const [products, setProducts] = useState([]);
   const [productFormVisible, setProductFormVisible] = useState(false);
+  
+  const getProducts = () => {
+    productService
+      .getAll()
+      .then(initialProducts => {
+        setProducts(initialProducts)
+      })
+  }
+  useEffect(getProducts, [])
 
   const toggleProductFormVisibility = () => {
     setProductFormVisible(!productFormVisible)
